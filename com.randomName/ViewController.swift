cfeebbe41095359bf737cf81c19dbb492c9fd441
Julian Pardo <julipardodiaz@hotmail.com>
2018-09-15 17:51:52 -0500

    Initial Commit

---
+++ com.randomName/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  com.randomName
+//
+//  Created by Julian Pardo on 15/09/18.
+//  Copyright © 2018 RandomName. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
